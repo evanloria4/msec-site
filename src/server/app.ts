c8f1b28cf592940448f4e155b7f800bd352e914d
Evan Loria <evan@cypressconnects.com>
2025-12-22 13:42:56 -0600

    Import cors and add pseudocode

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 
 const DIST_DIR = path.resolve(__dirname, '..', '..', 'dist');
 
 const app = express();
 
+/* ---------------- Middleware ---------------- */
+app.use(cors()); 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/* ---------------- Routes ---------------- */
+
+// Serve static files from the dist directory 
 app.use(express.static(DIST_DIR));
 
 export default app;
